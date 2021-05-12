9bbeb3a066425e97debba5613780a4fb68b507f9
leonardosoares1 <dev@doleonardo.com.br>
2021-05-11 22:12:05 -0300

    feat: send email

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,26 +1,36 @@
-
-
-  
-  
-
-
-const name = document.getElementById('inputName');
-const email = document.getElementById('inputEmail');
-const contactReason = document.getElementById('inputContactReason');
-
-const phone = document.getElementById('inputPhone');
+const inputName = document.getElementById('inputName');
+const inputEmail = document.getElementById('inputEmail');
+const inputPhone = document.getElementById('inputPhone');
+const inputReason = document.getElementById('inputContactReason');
 
 function phoneFormatted() {
-  
-  const phoneFormatted = phone.value
-  .replace(/[^0-9]/g, '')
-  .replace(/(\d{2})(\d)/, '($1) $2')
-  .replace(/(\d{4,5})(\d{4})/, '$1-$2')
-  .replace(/(-\d{4})\d+?$/, '$1');
+  const phoneFormatted = inputPhone.value
+    .replace(/[^0-9]/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4,5})(\d{4})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
 
-  phone.value = phoneFormatted
+  inputPhone.value = phoneFormatted
 }
 
-phone.onkeyup = phoneFormatted;
+inputPhone.onkeyup = phoneFormatted;
 
+function submitContact(event) {
+  event.preventDefault();
 
+  const client = inputName.value;
+  const email = inputEmail.value;
+  const phone = inputPhone.value;
+  const reason = inputReason.value;
+
+  //iniciar loading
+
+  fetch(`https://us-central1-hator-grupo-straceri.cloudfunctions.net/sendMail?email=${email}&client=${client}&phone=${phone}&reason=${reason}`)
+  .then(() => {
+    //finalizar loading;
+    // exibir algo falando que deu certo;
+  }).catch(() => {
+    //finalizar loading;
+    // exibir algo falando que deu errado;
+  })
+}
